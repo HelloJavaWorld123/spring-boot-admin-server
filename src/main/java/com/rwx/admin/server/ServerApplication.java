81fd70a3f9e5489fa2fc1668234c93b996b12520
xiaokuan.ren <renxiaokuan9428@duia.com>
2019-10-08 13:06:14 +0800

    简洁版admin

---
+++ src/main/java/com/rwx/admin/server/ServerApplication.java
@@ -0,0 +1,18 @@
+package com.rwx.admin.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author admin
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
