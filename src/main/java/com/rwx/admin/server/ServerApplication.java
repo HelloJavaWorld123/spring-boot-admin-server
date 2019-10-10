2e8645070d99068935f34af167515243179e275a
xiaokuan.ren <renxiaokuan9428@duia.com>
2019-10-10 22:21:33 +0800

    增加注册中心

--- src/main/java/com/rwx/admin/server/ServerApplication.java
+++ src/main/java/com/rwx/admin/server/ServerApplication.java
@@ -3,12 +3,14 @@ package com.rwx.admin.server;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author admin
  */
 @SpringBootApplication
 @EnableAdminServer
+@EnableDiscoveryClient
 public class ServerApplication {
 
 	public static void main(String[] args) {
