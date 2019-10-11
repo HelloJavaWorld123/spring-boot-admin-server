045836f73fd50741275710b50e1718bf73e0d9c6
xiaokuan.ren <renxiaokuan9428@duia.com>
2019-10-11 16:19:39 +0800

    增加注解

--- src/main/java/com/rwx/admin/server/ServerApplication.java
+++ src/main/java/com/rwx/admin/server/ServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * @author admin
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
+@EnableWebSecurity
 public class ServerApplication {
 
 	public static void main(String[] args) {
